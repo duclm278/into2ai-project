58a1ffb3e768358909c29a4bfff83c1bb79486dc
lmd278 <palered548@gmail.com>
2022-06-24 19:08:56 +0700

    Add bfs, dfs

--- bfs.py
+++ bfs.py
@@ -1,22 +1,32 @@
 import game
 import prune
 
+# Queue to store node
 class Fringe():
-    # Node: (state, actions)
+    # node = (state, action) = ((box, robot), action)
     def __init__(self):
-        raise NotImplementedError
+        self.fringe = []
+        self.active = set()
 
     def add(self, node):
-        raise NotImplementedError
+        node_state = node[0]
+        self.fringe.append(node)
+        self.active.add(node_state)
 
     def contains_state(self, state):
-        raise NotImplementedError
+        return state in self.active
 
     def empty(self):
-        raise NotImplementedError
+        # if len(self.fringe) == 0:
+        #     return True
+        return len(self.fringe) == 0
 
     def remove(self):
-        raise NotImplementedError
+        if not self.empty():
+            node_pop = self.fringe.pop(0)
+            node_state, node_action = node_pop
+            self.active.remove(node_state)
+            return node_pop
 
 class BFS():
     def __init__(self, level):
@@ -41,4 +51,50 @@ class BFS():
         walls = game.find_walls(board)
 
         # Initialize fringe to just the starting node
-        raise NotImplementedError
+        # node = (state , action) = ((box, robot), action)
+        start = ((boxes, robot), [None])
+        fringe = Fringe()
+        fringe.add(start)
+        
+        # Keep looping until solution found
+        while True:
+            # If nothing left in fringe, then no solution.
+            if fringe.empty():
+                return
+            else:
+                # Choose a node from the fringe
+                node = fringe.remove()
+                num_stored -= 1
+                # num_explored += 1
+                node_state = node[0]
+                node_boxes = node_state[0]
+                node_actions = node[1]
+                
+                # Don't mark explored here, mark it later.
+                # Here it isn't expanded to find neighbors.
+                # If node is the goal, then we have a solution.
+                # It will return immediately and won't enter for loop (find_moves).
+                if game.solved(node_boxes, goals):
+                    self.num_explored = len(explored)
+                    self.num_deadlocked = num_deadlocked
+                    self.max_stored = max_stored
+                    self.solution = "".join(node_actions[1:])
+                    return
+                
+                # Add neighbors to fringe
+                for action in game.find_moves(node_state, walls):
+                    # neighbor is a state
+                    neighbor = game.next_state(node_state, action)
+                    if not fringe.contains_state(neighbor):
+                        if neighbor not in explored:
+                            if prune.locked(neighbor[0], goals, walls):
+                                num_deadlocked += 1
+                            else:
+                                fringe.add((neighbor, node_actions + [action[0]]))
+                                num_stored += 1
+
+                # Here the node has been expanded.
+                # Mark explored here after entering for loop (find_moves)
+                explored.add(node_state)
+                num_stored += 1
+                max_stored = max(max_stored, num_stored)
